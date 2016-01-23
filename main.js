74d3ef8e874d72f1b64233b88f64ea6eec5afcdc
Simon Nistad <simon.nistad@hotmail.com>
2016-01-23 17:56:21 +0100

    Added more graphics.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 const MAX_SPEED = 350;
-const FALLING_SPEED = -400;
+const FALLING_SPEED = -650;
 const TURN_SPEED = 25;
 const TURN_UPDATE_INTERVA_MS = 10;
 const MAX_ANGLE = 30;
@@ -17,7 +17,9 @@ var mainstate = {
         game.stage.backgroundColor = '#71c5cf';
 
         game.load.image('bird', 'assets/skydiverSmall.png');
-        game.load.image('pipe', 'assets/cloudSmall.png');
+        game.load.image('pipe', 'assets/gate_gate.png');
+        game.load.image('backcloud', 'assets/backCloud.png');
+        game.load.image('logo', 'assets/logo.png');
 
         game.load.audio('jump', 'assets/jump.wav');
     },
@@ -31,13 +33,26 @@ var mainstate = {
 
         this.jumpSound = game.add.audio('jump');
 
-        this.bird = this.game.add.sprite(200, 200, 'bird');
+        this.backgroundClouds = game.add.group();
+        this.backgroundClouds.enableBody = true;
+        this.backgroundClouds.createMultiple(20, 'backcloud');
+
+
+        this.logo = this.game.add.sprite(game.world.centerX, 150, 'logo');
+        game.physics.arcade.enable(this.logo);
+        this.logo.anchor.setTo(0.5, 0.5);
+
+        this.controlText = game.add.text(game.world.centerX, 300, '0', {font: '20px Arial', fill: "#ffffff", fontWeight: 'bold'});
+        this.controlText.anchor.setTo(0.5, 0.5);
+        this.controlText.text = "PRESS ANY KEY TO START";
+
 
         this.pipes = game.add.group();
         this.pipes.enableBody = true; // add physics to the group
-        this.pipes.createMultiple(50, 'pipe'); // create X pipes
+        this.pipes.createMultiple(20, 'pipe'); // create X pipes
 
         // Add gravity to the bird
+        this.bird = this.game.add.sprite(game.world.centerX, 200, 'bird');
         game.physics.arcade.enable(this.bird);
         this.bird.body.velocity.x = 0;
         this.bird.anchor.setTo(0.5, 0.0);
@@ -57,13 +72,31 @@ var mainstate = {
         rightArrow.onDown.add(this.moveRightDown, this);
         rightArrow.onUp.add(this.moveRightUp, this);
 
+        this.backgroundCloudsTimer = game.time.events.loop(1000, this.addCloud, this);
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
 
         this.score = 0;
         this.labelScore = game.add.text(20, 20, '0', {font: '30px Arial', fill: "#ffffff"});
+
+        this.addCloud(false);
+        this.addCloud(false);
+        this.addCloud(false);
+        this.addCloud(false);
+        this.addCloud(false);
+
+        this.gameStarted = false;
+    },
+
+    startGame() {
+        this.gameStarted = true;
+        this.logo.body.velocity.y = -(MAX_SPEED * 2);
+        this.controlText.text = '';
     },
 
     moveLeftDown() {
+        if (!this.gameStarted) {
+            this.startGame();
+        }
         this.leftInterval = setInterval(() => {
 
             if (this.bird && this.bird.body.velocity.x > -MAX_SPEED) {
@@ -79,8 +112,10 @@ var mainstate = {
     },
 
     moveRightDown() {
+        if (!this.gameStarted) {
+            this.startGame();
+        }
         this.rightInterval = setInterval(() => {
-
             if (this.bird && this.bird.body.velocity.x < MAX_SPEED) {
                 this.bird.body.velocity.x += TURN_SPEED;
                 this.bird.angle = this.bird.body.velocity.x / MAX_SPEED * MAX_ANGLE;
@@ -108,11 +143,30 @@ var mainstate = {
             .overlap(this.bird, this.pipes, this.hitPipe, null, this);
     },
 
+    addCloud(bottom=true) {
+        let cloud = this.backgroundClouds.getFirstDead();
+
+        let y = 650;
+        if (!bottom) {
+            y = Math.floor(Math.random() * 650);
+        }
+        cloud.reset(Math.floor(Math.random() * 500) - 100, y);
+
+        let rand = Math.random();
+        cloud.alpha = rand;
+        cloud.scale.setTo(rand+0.2, rand+0.2);
+        cloud.body.velocity.y = -(Math.floor(rand * 150) + 200);
+
+        cloud.checkWorldBounds = true;
+        cloud.outOfBoundsKill = true;
+    },
+
     addOnePipe(x, y) {
         var pipe = this.pipes.getFirstDead();
 
         pipe.reset(x, y);
 
+        pipe.body.checkCollision.up = false;
         pipe.body.velocity.y = FALLING_SPEED;
 
         pipe.checkWorldBounds = true;
@@ -120,10 +174,13 @@ var mainstate = {
     },
 
     addRowOfPipes() {
+        if (!this.gameStarted) {
+            return;
+        }
         const hole = Math.floor(Math.random() * 5) + 1;
         for (var i = 0; i < 8; i++) {
             if (i != hole && i != hole + 1) {
-                this.addOnePipe(i * 60 + 10 , 600);
+                this.addOnePipe(i * 60 + 10 , 650);
             }
         }
         this.score += 1;
@@ -151,6 +208,10 @@ var mainstate = {
         this.bird.alive = false;
         this.bird.body.velocity.x = 1000;
 
+        game.time.events.remove(this.backgroundCloudsTimer);
+        this.backgroundClouds.forEachAlive(function(cloud) {
+            cloud.body.velocity.y = 0;
+        }, this);
         game.time.events.remove(this.timer);
         this.pipes.forEachAlive(function(pipe) {
             pipe.body.velocity.y = 0;
